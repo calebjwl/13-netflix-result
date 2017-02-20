9ab3bbd88f1bbc42a5d8f4cda2ecb793a5023402
Caleb Lee <caleb.gwl@gmail.com>
2017-02-20 16:19:07 -0600

    Finish js implementation

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
 import 'whatwg-fetch';
+import fillResults from './fill-in-result';
+  // import './data';
+const result = document.querySelector('.result-card');
+
+fetch(`http://netflixroulette.net/api/api.php?title=Attack%20on%20titan`)
+  .then(res => res.json()
+  ).then((data) => {
+    fillResults(result, data);
+  });
